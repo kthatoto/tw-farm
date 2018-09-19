f82ab59f02bca4bcba382918b9721b6ec99d1295
kthatoto <kthatoto@gmail.com>
2018-09-19 19:58:26 +0900

    Register outer command

---
+++ lib/commands/test.rb
@@ -0,0 +1,8 @@
+module Twfarm
+  class Test < Thor::Group
+
+    def test
+      puts :test
+    end
+  end
+end
--- lib/twfarm.rb
+++ lib/twfarm.rb
@@ -1,7 +1,10 @@
 require "thor"
 require "twfarm/version"
 
+require "commands/test"
+
 module Twfarm
   class CLI < Thor
+    register Twfarm::Test, "test", "test", "Puts test"
   end
 end
