e5daa3581ea104b9deabe0e7ba7a0b1c4996c228
kthatoto <kthatoto@gmail.com>
2018-09-21 15:24:20 +0900

    Added subcommand in shop

--- lib/app/commands/shop.rb
+++ lib/app/commands/shop.rb
@@ -1,9 +1,43 @@
 module Twfarm
-  class Shop < Thor::Group
+  class Shop < Thor
 
+    default_command :shop
+
+    desc "", "Show shop items"
     def shop
-      pp Master::ShopItem.all
-      pp Master::Plant.all
+      get_shop_items
+      show_items
+    end
+
+    desc "buy [id]", "Buy items"
+    def buy(id = false)
+      get_shop_items
+      Twfarm.twputs "You have bought id:#{id} item!"
+    end
+
+    private
+    def get_shop_items
+      master_shop_items = Master::ShopItem.all
+      @shop_items = {
+        plants: [],
+        fertilizer: [],
+      }
+      @shop_items[:plants] = master_shop_items.select{|row|
+        row["shop_itemable_type"] == "plant"
+      }.map{|row|
+        Master::Plant.get_by_id(row["shop_itemable_id"]).merge(
+          {"shop_item_id" => row["master_id"]}
+        )
+      }
+    end
+
+    def show_items
+      Twfarm.twputs Twfarm.greenen("[Shop]")
+      Twfarm.twputs Twfarm.greenen("{Plant Seeds}"), 1
+      @shop_items[:plants].each do |item|
+        line = "#{item["shop_item_id"]} => name:#{item["name"]}, price:#{item["seed_price"]}"
+        Twfarm.twputs line, 2
+      end
     end
   end
 end
